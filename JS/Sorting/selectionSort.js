0e8dc99bb5039f4ef3ede46776a33885bc2e12f4
abhilash.krishnan <abhilash.krishnan@digitalturbine.com>
2025-09-19 20:51:52 +0530

    updated code for selection sort

--- JS/Sorting/selectionSort.js
+++ JS/Sorting/selectionSort.js
@@ -1,34 +1,41 @@
 /*
-Bubble Sort is a simple sorting algorithm that repeatedly compares adjacent elements in a list and swaps them if 
-they are in the wrong order. This process continues until the list becomes completely sorted.
+Selection Sort is one of the simplest sorting algorithms. 
+It works by repeatedly finding the smallest (or largest) element 
+from the unsorted portion of the list and moving it to the correct position in the sorted portion.
 */
 
 
 function bubbleSort(arr, callbackFn) {
     
     function defaultCompare(a, b){
-        return a > b ? 1 : a === b ? 0 : -1
+        return parseInt(a) > parseInt(b) ? 1 : a === b ? 0 : -1
     }
 
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
+        let smallest  = i;
+        for (let j = i + 1; j < arr.length; j++) {
             let comapare;
-            if(arr[j + 1] === undefined){
-                continue;
-            }
             if (typeof callbackFn !== 'function') {
-                comapare = defaultCompare(arr[j], arr[j + 1])
+                comapare = defaultCompare(arr[smallest], arr[j])
             } else {
-                comapare = callbackFn(arr[j], arr[j + 1])
+                comapare = callbackFn(arr[smallest], arr[j])
             }
             if (comapare > 0) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                smallest = j;
             } 
+        }
+        
+        if(smallest !== i) {
+            const temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
     }
     return arr;
 }
 
+// var nums = [4, 20, 12, 10, 7, 9];
+// console.log(bubbleSort(nums))
 
 // var kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
 // function strComp(a, b) {
@@ -39,27 +46,27 @@ function bubbleSort(arr, callbackFn) {
 // console.log(bubbleSort(kitties, strComp))
 
 
-var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
-console.log(bubbleSort(nums))
+// var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+// console.log(bubbleSort(nums))
 
-// var moarKittyData = [{
-//   name: "LilBub",
-//   age: 7
-// }, {
-//   name: "Garfield",
-//   age: 40
-// }, {
-//   name: "Heathcliff",
-//   age: 45
-// }, {
-//   name: "Blue",
-//   age: 1
-// }, {
-//   name: "Grumpy",
-//   age: 6
-// }];
+var moarKittyData = [{
+  name: "LilBub",
+  age: 7
+}, {
+  name: "Garfield",
+  age: 40
+}, {
+  name: "Heathcliff",
+  age: 45
+}, {
+  name: "Blue",
+  age: 1
+}, {
+  name: "Grumpy",
+  age: 6
+}];
  
-// function oldestToYoungest(a, b) {
-//   return b.age - a.age;
-// }
-// console.log(bubbleSort(moarKittyData, oldestToYoungest))
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+console.log(bubbleSort(moarKittyData, oldestToYoungest))
